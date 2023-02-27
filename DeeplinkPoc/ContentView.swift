adf885718f07eeda0edace479dc1edc9b7d2c72e
devanshuchaudhary <devanshu88@gmail.com>
2023-02-27 09:20:06 +0530

    Fixed navigationstack

--- DeeplinkPoc/ContentView.swift
+++ DeeplinkPoc/ContentView.swift
@@ -8,40 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-	@ObservedObject var numberVM = AddNumbers()
-	var body: some View {
-		NavigationStack {
-			List(numberVM.numbers) { number in
-				NavigationLink(destination: NumberDetailView(number: number)) {
-					Text(String(number.number))
-				}
-			}
-		}
-		.onAppear {
-			numberVM.creatNumbers()
-		}
-	}
+    
+    @ObservedObject var numberVM = AddNumbers()
+
+    var body: some View {
+            List(numberVM.numbers) { number in
+                NavigationLink(value: number.number) {
+                    Text(String(number.number))
+                }
+            }
+        .onAppear {
+            numberVM.creatNumbers()
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-			.environmentObject(AddNumbers())
-	}
+    static var previews: some View {
+        ContentView()
+            .environmentObject(AddNumbers())
+    }
 }
 
 struct Number: Identifiable {
-	var id = UUID()
-	let number: Int
+    var id = UUID()
+    let number: Int
 }
 
 
 class AddNumbers: ObservableObject {
-	@Published var numbers = [Number]()
-	
-	func creatNumbers() {
-		for number in 0...100 {
-			numbers.append(Number(number: number))
-		}
-	}
+    @Published var numbers = [Number]()
+    
+    func creatNumbers() {
+        for number in 0...100 {
+            numbers.append(Number(number: number))
+        }
+    }
 }
--- DeeplinkPoc/DeeplinkPocApp.swift
+++ DeeplinkPoc/DeeplinkPocApp.swift
@@ -9,26 +9,42 @@ import SwiftUI
 
 @main
 struct DeeplinkPocApp: App {
-	@State var deeplinkTarget: DeeplinkManager.DeeplinkTarget?
-
-	var body: some Scene {
-		WindowGroup {
-			Group {
-				switch self.deeplinkTarget {
-				case .home:
-					ContentView()
-				case .details(let queryInfo):
-					NumberDetailView(number: Number(number: 6))
-				case .none:
-					ContentView()
-				}
-			}
-			.onOpenURL { url in
-				let deeplinkManager = DeeplinkManager()
-				let deeplink = deeplinkManager.manage(url)
-				self.deeplinkTarget = deeplink
-			}
-			.environmentObject(AddNumbers())
-		}
-	}
+    
+    @State private var presentedNums = [Int]()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack(path: $presentedNums) {
+                ContentView()
+                    .navigationDestination(for: Int.self) { num in
+                        NumberDetailView(number: Number(number: num))
+                    }
+            }
+            .onOpenURL { url in
+                
+                let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+                
+                if let urlComponents {
+                    
+                    if url.pathComponents.contains("number") {
+                        
+                        if let queryItems = urlComponents.queryItems {
+                            
+                            for query in queryItems {
+                                
+                                if let value = query.value {
+                                    
+                                    if let numValue = Int(value) {
+                                        
+                                        presentedNums.append(numValue)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .environmentObject(AddNumbers())
+        }
+    }
 }
